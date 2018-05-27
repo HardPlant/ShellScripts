daae7df7eb901470872e42bb69c8d204ff1d505f
HardPlant <abc7988se@naver.com>
2018-05-27 03:44:29 -0700

    added autobuild
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- fabric_clone.sh
+++ fabric_clone.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 mkdir -p $GOPATH/src/github.com/hyperledger
 cd $GOPATH/src/github.com/hyperledger
 scp -r -P 42000 kiiren@168.131.42.48:/home/kiiren/repos/build/fabric  $(pwd)
+cd fabric
+make native
