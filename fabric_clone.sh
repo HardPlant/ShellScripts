162cc3e503766bc7782ce775b46a331c0d0da37d
HardPlant <abc7988se@naver.com>
2018-05-27 03:39:50 -0700

    pwd
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- fabric_clone.sh
+++ fabric_clone.sh
@@ -1,3 +1,3 @@
 mkdir -p $GOPATH/src/github.com/hyperledger
 cd $GOPATH/src/github.com/hyperledger
-scp -r -P 42000 kiiren@168.131.42.48:/home/kiiren/repos/build/fabric  ./fabric
+scp -r -P 42000 kiiren@168.131.42.48:/home/kiiren/repos/build/fabric  $(pwd)
