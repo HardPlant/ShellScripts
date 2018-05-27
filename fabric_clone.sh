4628ad33fb8dd37e4a1ac72d522dad93ef16d0f3
HardPlant <abc7988se@naver.com>
2018-05-27 04:53:19 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- fabric_clone.sh
+++ fabric_clone.sh
@@ -2,11 +2,14 @@
 
 mkdir -p $GOPATH/src/github.com/hyperledger
 sudo mkdir -p /etc/hyperledger
-mv core.yaml /etc/hyperledger
+cp core.yaml /etc/hyperledger
+
+sudo mkdir -p /var/hyperledger
+cp core.yaml /var/hyperledger
 
 cd $GOPATH/src/github.com/hyperledger
 scp -r -P 42000 kiiren@168.131.42.48:/home/kiiren/repos/build/fabric  $(pwd)
 cd fabric
 make native
-sudo mkdir -p /etc/hyperledger
-sudo mv -f .build/bin/* /etc/hyperledger
+sudo mv -f .build/bin/peer /etc/hyperledger
+sudo mv -f .build/bin/orderer /var/hyperledger
