46fa65f1b1b0cc434af09a541517133ae6e69076
HardPlant <abc7988se@naver.com>
2018-05-28 00:53:30 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- #.sh
+++ #.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /etc/hyperledger/admintls
+mkdir -p /etc/hyperledger/adminmsp
 
-scp kiiren@192.168.30.155:/tmp/e2e_cli/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/ca.crt /etc/hyperledger/admintls
+scp kiiren@192.168.30.155:/tmp/e2e_cli/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp /etc/hyperledger/adminmsp
 
--- 4.create_channel.sh
+++ 4.create_channel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-scp -r kiiren@192.168.30.155:/tmp/crypto-config/ordererOrganizations/example.com/msp/tlscacerts /etc/hyperledger/orderercert
+#scp -r kiiren@192.168.30.155:/tmp/crypto-config/ordererOrganizations/example.com/msp/tlscacerts /etc/hyperledger/orderercert
 CHANNEL_NAME=mychannel
 CORE_PEER_TLS_ENABLED=true
 ORDERER_CA=/etc/hyperledger/orderercert/tlsca.example.com-cert.pem
