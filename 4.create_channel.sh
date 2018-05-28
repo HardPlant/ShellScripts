6ef5a368cd188671d55617319e6cf3d56ff29803
HardPlant <abc7988se@naver.com>
2018-05-27 23:57:18 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

---
+++ 4.create_channel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+scp -r kiiren@192.168.30.155:/tmp/crypto-config/ordererOrganizations/example.com/msp/tlscacerts /etc/hyperledger/orderercert
+ORDERER_CA=/etc/hyperledger/orderercert/tlsca.example.com-cert.pem
+peer channel create -o orderer.example.com:7050 \
+-c $CHANNEL_NAME \
+-f ./channel-artifacts/channel.tx \
+--tls $CORE_PEER_TLS_ENABLED \
+--cafile $ORDERER_CA
